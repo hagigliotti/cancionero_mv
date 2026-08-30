73299449212d5b2316abeaba5a232357426b43b2
Hernando Gigliotti <hagigliotti@gmail.com>
2026-08-30 16:34:36 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -445,16 +445,16 @@ function renderRevisadoPersonas(value) {
 // ===================== MODALES DINÁMICOS ===================== Para abrir modal Acerca de... desde otro archivo
 async function cargarModales() {
   const modales = [
-    "modals/info.html?v=147",
-    "modals/revised.html?v=147",
-    "modals/people.html?v=147",
-    "modals/valores.html?v=147",
-    "modals/share.html?v=147",
-    "modals/contacto.html?v=147",
-    "modals/afinometro.html?v=147",
-    "modals/biblioteca.html?v=147",
-    "modals/listas.html?v=147",
-    "modals/notepad.html?v=147"
+    "modals/info.html?v=148",
+    "modals/revised.html?v=148",
+    "modals/people.html?v=148",
+    "modals/valores.html?v=148",
+    "modals/share.html?v=148",
+    "modals/contacto.html?v=148",
+    "modals/afinometro.html?v=148",
+    "modals/biblioteca.html?v=148",
+    "modals/listas.html?v=148",
+    "modals/notepad.html?v=148"
   ];
 
   for (const path of modales) {
